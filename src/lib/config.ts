c7c0e7d8a6ea1b0a7cf258487623ca7042c01c80
Jerome Kenway <jeromekenway@gmail.com>
2023-10-27 14:29:17 +0100

    fix: footer links

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -3,7 +3,8 @@ export const ABBR = 'ETLW'
 export const PODCAST_NAME = 'Ears To Learn With'
 export const CONTACT_EMAIL = 'etlwrecords@gmail.com'
 export const CONTACT_NUMBER = '0788180461'
-export const TWITTER = 'etlw_pod'
-export const INSTAGRAM = 'etlw_pod'
-export const DESCRIPTION =
-  `The ${PODCAST_NAME} podcast is a multi content endeavour that will provide exciting, unique and exclusive musical collaborations for people to stream, buy and enjoy`
+export const TWITTER = ''
+export const YOUTUBE = ''
+export const INSTAGRAM = 'etlwpod'
+export const FACEBOOK = ''
+export const DESCRIPTION = `The ${PODCAST_NAME} podcast is a multi content endeavour that will provide exciting, unique and exclusive musical collaborations for people to stream, buy and enjoy`
